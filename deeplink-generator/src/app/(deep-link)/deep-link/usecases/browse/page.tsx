84b9b9107eeeab8309f30abae24f591e3b9136f8
Sonali Shakya <sonalishakya@Admins-MacBook-Air-3.local>
2025-01-20 14:43:04 +0530

    Build fixed

--- deeplink-generator/src/app/(deep-link)/deep-link/usecases/browse/page.tsx
+++ deeplink-generator/src/app/(deep-link)/deep-link/usecases/browse/page.tsx
@@ -1,14 +1,14 @@
 import {
-	Accordion,
-	AccordionDetails,
-	AccordionSummary,
-	Box,
-	Button,
-	Radio,
-	RadioGroup,
-	TextField,
-	Toolbar,
-	Typography,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Button,
+  Radio,
+  RadioGroup,
+  TextField,
+  Toolbar,
+  Typography,
 } from "@mui/material";
 import React from "react";
 
@@ -20,105 +20,107 @@ import Link from "next/link";
 import { Template } from "@prisma/client";
 
 const SelectUsecasePage = async ({
-	searchParams,
+  searchParams,
 }: {
-	searchParams: Promise<{ category: string; subcategory: string }>;
+  searchParams: Promise<{ category: string; subcategory: string }>;
 }) => {
-	const { category, subcategory } = await searchParams;
-	const templates = await getTemplateByCategoryAndSubcategory(
-		category,
-		subcategory
-	);
+  const { category, subcategory } = await searchParams;
+  const templates = await getTemplateByCategoryAndSubcategory(
+    category,
+    subcategory
+  );
 
-	if (!Array.isArray(templates))
-		redirect(`/deep-link/usecases/create/${(templates as Template).id}`);
+  if (!Array.isArray(templates))
+    redirect(`/deep-link/usecases/create/${(templates as Template).id}`);
 
-	const handleSelection = async (form: FormData) => {
-		"use server";
-		redirect(
-			`/deep-link/usecases/create/${form.get(
-				"templateId"
-			)}?category=${category}&subcategory=${subcategory}`
-		);
-	};
+  const handleSelection = async (form: FormData) => {
+    "use server";
+    redirect(
+      `/deep-link/usecases/create/${form.get(
+        "templateId"
+      )}?category=${category}&subcategory=${subcategory}`
+    );
+  };
 
-	return (
-		<>
-			<Toolbar />
-			<CustomHeading
-				heading="Select Template"
-				breadcrumb={[{ name: "Home", link: "/" }]}
-			/>
-			<Toolbar />
-			{Array.isArray(templates) && templates.length > 0 ? (
-				<Form action={handleSelection}>
-					<TextField
-						fullWidth
-						title="Search Templates"
-						placeholder="Search Templates..."
-					/>
-					<RadioGroup name="templateId" defaultValue={templates[0].id}>
-						{templates.map((template) => (
-							<Box
-								key={template.id}
-								sx={{
-									my: 1,
-									width: "100%",
-									display: "flex",
-									justifyContent: "flex-start",
-									alignItems: "center",
-								}}
-							>
-								<Radio value={template.id} required />
-								<Accordion sx={{ ml: 1, width: "100%" }}>
-									<AccordionSummary>
-										<Typography>Usecase: {template.name}</Typography>
-									</AccordionSummary>
-									<AccordionDetails>
-										<Typography>
-											{JSON.stringify(template.value, undefined, 2)}
-										</Typography>
-										<Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-											<Button
-												variant="contained"
-												sx={{ textTransform: "none" }}
-											>
-												Verify
-											</Button>
-										</Box>
-									</AccordionDetails>
-								</Accordion>
-							</Box>
-						))}
-					</RadioGroup>
-					<Box
-						sx={{
-							display: "flex",
-							justifyContent: "flex-end",
-							alignItems: "center",
-						}}
-					>
-						<CustomContainedButtom
-							type="submit"
-							disabled={templates.length === 0}
-						>
-							Create Usecase
-						</CustomContainedButtom>
-					</Box>
-				</Form>
-			) : (
-				<>
-					<Typography variant="body2" mb={3}>
-						There are not templates defined for this category and sub category
-						currently.
-					</Typography>
-					<Link href="/">
-						<CustomContainedButtom>Back to Home</CustomContainedButtom>
-					</Link>
-				</>
-			)}
-		</>
-	);
+  return (
+    <>
+      <Toolbar />
+      <CustomHeading
+        heading="Select Template"
+        breadcrumb={[{ name: "Home", link: "/" }]}
+      />
+      <Toolbar />
+      {Array.isArray(templates) && templates.length > 0 ? (
+        <Form action={handleSelection}>
+          <TextField
+            fullWidth
+            title="Search Templates"
+            placeholder="Search Templates..."
+          />
+          <RadioGroup name="templateId" defaultValue={templates[0].id}>
+            {templates.map((template) => (
+              <Box
+                key={template.id}
+                sx={{
+                  my: 1,
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                }}
+              >
+                <Radio value={template.id} required />
+                <Accordion sx={{ ml: 1, width: "100%" }}>
+                  <AccordionSummary>
+                    <Typography>Usecase: {template.name}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography>
+                      {JSON.stringify(template.value, undefined, 2)}
+                    </Typography>
+                    <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                      <Button
+                        variant="contained"
+                        sx={{ textTransform: "none" }}
+                      >
+                        Verify
+                      </Button>
+                    </Box>
+                  </AccordionDetails>
+                </Accordion>
+              </Box>
+            ))}
+          </RadioGroup>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              alignItems: "center",
+            }}
+          >
+            <CustomContainedButtom
+              buttonBaseProps={{
+                type: "submit",
+                disabled: templates.length === 0,
+              }}
+            >
+              Create Usecase
+            </CustomContainedButtom>
+          </Box>
+        </Form>
+      ) : (
+        <>
+          <Typography variant="body2" mb={3}>
+            There are not templates defined for this category and sub category
+            currently.
+          </Typography>
+          <Link href="/">
+            <CustomContainedButtom>Back to Home</CustomContainedButtom>
+          </Link>
+        </>
+      )}
+    </>
+  );
 };
 
 export default SelectUsecasePage;
